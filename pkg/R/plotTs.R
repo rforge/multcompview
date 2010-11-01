40e3748cb39031ec75a9dc8d0904d1ebdd3dfe0d
spencerg <spencerg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-11-01 14:20:38 +0000

    fix plotTs to respond to cex.axis & add examples to multcompTs / Letters
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/multcompview@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plotTs.R
+++ pkg/R/plotTs.R
@@ -1,10 +1,9 @@
 "plotTs" <-
 function(obj, at, width,
-      horizontal, col, add, lwd, label.levels, 
+      horizontal, col, add, lwd, label.levels,
       label.groups, T.base, orientation="",
       ...){
-  if(orientation=="reverse")
-    width[,] <- width[, 3:1]  
+  if(orientation=="reverse") width[,] <- width[, 3:1]
   lvl.rng <- range(at)
   gp.rng <- range(width)
   n <- dim(obj)[1]
@@ -23,15 +22,15 @@ function(obj, at, width,
       for(i in 1:k){
         for(j in 1:n){
           if(obj[j,i]<0)next
-#         Top of the "T"          
+#         Top of the "T"
           rect(at[j, "bottom"], width[i, "center"],
                at[j, "top"], width[i, "top"],
-               density=(-1), col=col[i], lty="blank") 
+               density=(-1), col=col[i], lty="blank")
           if(obj[j,i]>0)
             rect(at.5[j, 1], width[i, "bottom"],
                  at.5[j, 2], width[i, "top"],
                  density=(-1), col=col[i], lty="blank")
-#           Leg(s) of the "T"          
+#           Leg(s) of the "T"
         }
 #       Bridge across the top in case of gaps
         lines(range(at[obj[, i]>(-1), ]),
@@ -46,7 +45,7 @@ function(obj, at, width,
       }
       if(!is.na(label.groups))
         text(lvl.rng[1]-label.groups*diff(lvl.rng),
-             width[, "center"], 
+             width[, "center"],
              dimnames(obj)[[2]])
     }
     else{
@@ -55,15 +54,15 @@ function(obj, at, width,
       for(i in 1:k){
         for(j in 1:n){
           if(obj[j,i]<0)next
-#         Top of the "T"          
+#         Top of the "T"
           rect(width[i, "center"], at[j, "bottom"],
                width[i, "top"], at[j, "top"],
-               density=(-1), col=col[i], lty="blank") 
+               density=(-1), col=col[i], lty="blank")
           if(obj[j,i]>0)
-            rect(width[i, "bottom"],at.5[j, 1], 
-                 width[i, "top"],at.5[j, 2], 
+            rect(width[i, "bottom"],at.5[j, 1],
+                 width[i, "top"],at.5[j, 2],
                  density=(-1), col=col[i], lty="blank")
-#           Leg(s) of the "T"          
+#           Leg(s) of the "T"
         }
 #       Bridge across the top in case of gaps
         lines(rep(width[i, "top"],2),
@@ -71,14 +70,16 @@ function(obj, at, width,
               col=col[i], lwd=lwd)
       }
 #     Labels?
-      if(!is.na(label.levels))
+      dots <- list(...)
+      cex <- (if(is.null(dots$cex.axis)) 1 else dots$cex.axis)
+      if(!is.na(label.levels)){
         text(gp.rng[1]-label.levels*diff(gp.rng),
-             at[, "center"],
-             dimnames(obj)[[1]])
+             at[, "center"], dimnames(obj)[[1]], cex=cex)
+      }
       if(!is.na(label.groups))
-        text(width[, "center"], 
+        text(width[, "center"],
              lvl.rng[1]-label.groups*diff(lvl.rng),
-             dimnames(obj)[[2]])
+             dimnames(obj)[[2]], cex=cex)
     }
   }
   "Done"
